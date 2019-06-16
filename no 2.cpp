b5b3907e5d26a5cdb018a223b6f79420fb7e3a8e
Arizal-Arkan <50759888+Arizal-Arkan@users.noreply.github.com>
2019-06-16 08:03:25 +0700

    Create no 2.cpp

---
+++ no 2.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+    int mentok;
+
+    printf("Input : ");
+    scanf("%d", &mentok);
+    for(int i = 1; i <= mentok; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            //bilangan prima
+    printf(for (a=2;a<=batas;a++){
+            prim=1;
+        for (b=2;b<a;b++){
+   if(a%b==0)
+   {
+    prim=0;
+   }
+  }
+  if(prim==1)
+  {
+   cout<<a<<" ";
+  }
+        }
+            );
+        }
+        printf("\n");
+    }
+    return 0;
+}
