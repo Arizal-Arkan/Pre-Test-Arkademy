2dc1237286b174705a191b7fbe1411c3d6ebeb73
Arizal-Arkan <50759888+Arizal-Arkan@users.noreply.github.com>
2019-06-16 08:02:34 +0700

    Create no 1.js

---
+++ no 1.js
@@ -0,0 +1,14 @@
+function bayar (jumlah){
+    var uang = 2500;
+ 	var mie;
+
+ 		if(uang > 1){
+ 	uang * 2;
+}
+  		else if(mie > 4){
+    mie + 1;    
+}
+  		else if(jumlah){
+	jumlah = 25;
+}
+     }
